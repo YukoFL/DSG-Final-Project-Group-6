21fa8c3146ae38229cbd8b2ece78d8c889f4fe6a
YukoFL <evan.pokemine@gmail.com>
2025-12-06 16:52:59 +0700

    Updated code with weight over weight Alarm LED
    Patch notes
    >Added Weight and Overweight if Weight > Overweight elevator stop until resolved
    >Added Alarm and LED that trigger if Overweight is reached
    >Added Call button similar to calling an elevator staff during emergency which trigger call mode

--- MypartFinpro/floor request manage.vhdl
+++ MypartFinpro/floor request manage.vhdl
@@ -2,38 +2,82 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity floor_request_manager is
-    Port (
-        clk            : in  STD_LOGIC;
-        reset          : in  STD_LOGIC;
-
-        add_req        : in  STD_LOGIC_VECTOR(7 downto 0); 
-        cancel_req     : in  STD_LOGIC_VECTOR(7 downto 0); 
-        emergency_stop : in  STD_LOGIC;                    
-
-        active_floors  : out STD_LOGIC_VECTOR(7 downto 0)  
+    Port(
+        clk: in STD_LOGIC;
+        reset: in STD_LOGIC;
+        add_req: in STD_LOGIC_VECTOR(7 downto 0);
+        cancel_req: in STD_LOGIC_VECTOR(7 downto 0);
+        emergency_stop: in STD_LOGIC;
+        current_weight: in INTEGER;
+        call_button: in STD_LOGIC;
+        call_mode: out STD_LOGIC;
+        overweight: out STD_LOGIC;
+        alarm: out STD_LOGIC;
+        led_out: out STD_LOGIC;
+        active_floors: out STD_LOGIC_VECTOR(7 downto 0)
     );
 end floor_request_manager;
 
 architecture Behavioral of floor_request_manager is
-    signal reg_floors : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+    signal reg_floors: STD_LOGIC_VECTOR(7 downto 0):=(others=>'0');
+    signal overweight_flag: STD_LOGIC:='0';
+    signal call_flag: STD_LOGIC:='0';
+    signal blink_counter: INTEGER:=0;
+    signal led_state: STD_LOGIC:='0';
 begin
 
-    process(clk, reset)
-    begin
-        if reset = '1' then
-            reg_floors <= (others => '0');
+process(clk,reset)
+begin
+    if reset='1' then
+        reg_floors<=(others=>'0');
+        overweight_flag<='0';
+        call_flag<='0';
+        led_state<='0';
+        blink_counter<=0;
+    elsif rising_edge(clk) then
 
-        elsif rising_edge(clk) then
-            if emergency_stop = '1' then
-                reg_floors <= (others => '0');
+        if call_button='1' then
+            call_flag<='1';
+        end if;
 
+        if current_weight<=2000 then
+            call_flag<='0';
+        end if;
+
+        if current_weight>2000 then
+            overweight_flag<='1';
+            reg_floors<=reg_floors;
+        else
+            overweight_flag<='0';
+            if call_flag='1' then
+                reg_floors<=reg_floors;
             else
-                reg_floors <= (reg_floors or add_req) and (not cancel_req);
-
+                if emergency_stop='1' then
+                    reg_floors<=(others=>'0');
+                else
+                    reg_floors<=(reg_floors or add_req) and (not cancel_req);
+                end if;
             end if;
         end if;
-    end process;
 
-    active_floors <= reg_floors;
+        if overweight_flag='1' then
+            blink_counter<=blink_counter+1;
+            if blink_counter=5000000 then
+                led_state<=not led_state;
+                blink_counter<=0;
+            end if;
+        else
+            led_state<='0';
+            blink_counter<=0;
+        end if;
+
+    end if;
+end process;
+
+overweight<=overweight_flag;
+alarm<=overweight_flag;
+led_out<=led_state;
+call_mode<=call_flag;
+active_floors<=reg_floors;
 
 end Behavioral;
--- MypartFinpro/tb_floorRequestManage.vhdl
+++ MypartFinpro/tb_floorRequestManage.vhdl
@@ -5,45 +5,73 @@ entity tb_floor_request_manager is
 end tb_floor_request_manager;
 
 architecture sim of tb_floor_request_manager is
-    signal clk            : STD_LOGIC := '0';
-    signal reset          : STD_LOGIC := '0';
-    signal add_req        : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
-    signal cancel_req     : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
-    signal emergency_stop : STD_LOGIC := '0';
-    signal active_floors  : STD_LOGIC_VECTOR(7 downto 0);
-
+    signal clk: STD_LOGIC:='0';
+    signal reset: STD_LOGIC:='0';
+    signal add_req: STD_LOGIC_VECTOR(7 downto 0):=(others=>'0');
+    signal cancel_req: STD_LOGIC_VECTOR(7 downto 0):=(others=>'0');
+    signal emergency_stop: STD_LOGIC:='0';
+    signal current_weight: INTEGER:=0;
+    signal call_button: STD_LOGIC:='0';
+    signal call_mode: STD_LOGIC;
+    signal overweight: STD_LOGIC;
+    signal alarm: STD_LOGIC;
+    signal led_out: STD_LOGIC;
+    signal active_floors: STD_LOGIC_VECTOR(7 downto 0);
 begin
-    uut: entity work.floor_request_manager
-        port map (
-            clk => clk,
-            reset => reset,
-            add_req => add_req,
-            cancel_req => cancel_req,
-            emergency_stop => emergency_stop,
-            active_floors => active_floors
-        );
 
-    clk <= not clk after 5 ns;
+uut: entity work.floor_request_manager
+    port map(
+        clk=>clk,
+        reset=>reset,
+        add_req=>add_req,
+        cancel_req=>cancel_req,
+        emergency_stop=>emergency_stop,
+        current_weight=>current_weight,
+        call_button=>call_button,
+        call_mode=>call_mode,
+        overweight=>overweight,
+        alarm=>alarm,
+        led_out=>led_out,
+        active_floors=>active_floors
+    );
 
-    process
-    begin
-        reset <= '1';
-        wait for 20 ns;
-        reset <= '0';
+clk<=not clk after 5 ns;
 
-        add_req <= "00001000";
-        wait for 10 ns;
-        add_req <= (others => '0');
+process
+begin
+    reset<='1';
+    wait for 20 ns;
+    reset<='0';
 
-        cancel_req <= "00001000";
-        wait for 10 ns;
-        cancel_req <= (others => '0');
+    add_req<="00001000";
+    wait for 10 ns;
+    add_req<=(others=>'0');
 
-         emergency_stop <= '1';
-        wait for 10 ns;
-        emergency_stop <= '0';
+    current_weight<=2500;
+    wait for 20 ns;
 
-        wait;
-    end process;
+    add_req<="00000010";
+    wait for 10 ns;
+    add_req<=(others=>'0');
+
+    current_weight<=1500;
+    wait for 20 ns;
+
+    call_button<='1';
+    wait for 10 ns;
+    call_button<='0';
+
+    add_req<="00000001";
+    wait for 10 ns;
+    add_req<=(others=>'0');
+
+    current_weight<=2500;
+    wait for 20 ns;
+
+    current_weight<=1500;
+    wait for 20 ns;
+
+    wait;
+end process;
 
 end sim;
